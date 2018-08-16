9e7545ad399c05dde20838fee5ecd4e51f10f8e4
Максим Тарасов <yousan4ik@gmail.com>
2018-08-16 18:54:30 +0300

    Update index.php

--- sandbox/src/index.php
+++ sandbox/src/index.php
@@ -1,8 +1,9 @@
 <?php 
 
 use php\android{ UXView, UXMobileApplication };
+use php\gui\UXButton;
 
 $view = new UXView(UXView::HOME_NAME);
-
+$view->center = new UXButton("test jphp");
 UXMobileApplication::addView(UXView::HOME_NAME, $view);
  
