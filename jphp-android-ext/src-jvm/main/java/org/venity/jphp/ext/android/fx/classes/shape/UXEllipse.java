c8dd0c9331039be4d9ef63df319fe6d14c960501
Максим Тарасов <yousan4ik@gmail.com>
2018-08-21 13:56:03 +0300

    Update UXEllipse.java

--- jphp-android-ext/src-jvm/main/java/org/venity/jphp/ext/android/fx/classes/shape/UXEllipse.java
+++ jphp-android-ext/src-jvm/main/java/org/venity/jphp/ext/android/fx/classes/shape/UXEllipse.java
@@ -8,7 +8,7 @@ import php.runtime.annotation.Reflection.Signature;
 import php.runtime.env.Environment;
 import php.runtime.reflection.ClassEntity;
 
-@Name(JavaFXExtension.NS + "shape\\UXEllipse")
+@Name("php\\gui\\shape\\UXEllipse")
 public class UXEllipse extends UXShape<Ellipse> {
     interface WrappedInterface {
         @Property double radiusX();
