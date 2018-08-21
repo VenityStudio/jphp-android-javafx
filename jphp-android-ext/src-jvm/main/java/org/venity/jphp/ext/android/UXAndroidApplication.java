e3e563b1a7fb3abd5f8e80108597b1b06afe3945
Максим Тарасов <yousan4ik@gmail.com>
2018-08-21 15:31:55 +0300

    Update UXAndroidApplication.java

--- jphp-android-ext/src-jvm/main/java/org/venity/jphp/ext/android/UXAndroidApplication.java
+++ jphp-android-ext/src-jvm/main/java/org/venity/jphp/ext/android/UXAndroidApplication.java
@@ -3,21 +3,24 @@ package org.venity.jphp.ext.android;
 import com.gluonhq.charm.glisten.application.MobileApplication;
 import java.io.IOException;
 import javafx.scene.Scene;
+import com.gluonhq.charm.glisten.control.ExceptionDialog;
 
 public class UXAndroidApplication extends MobileApplication {
 
     @Override
     public void postInit(Scene scene) {
-	    try {
-	StandaloneAndroidLoader loader = new StandaloneAndroidLoader();
-	System.out.println("Starting JPHP android application");
-        System.out.println("Code with love by venity");
+	try {
+	    StandaloneAndroidLoader loader = new StandaloneAndroidLoader();
+	    System.out.println("Starting JPHP android application");
+            System.out.println("Code with love by venity");
 
-        loader.setClassLoader(getClass().getClassLoader());
-        loader.loadLibrary();
-        loader.run();
-		    } catch (Exception e) {
-		        e.printStackTrace(); 
-		    }
+            loader.setClassLoader(getClass().getClassLoader());
+            loader.loadLibrary();
+            loader.run();
+	} catch (Exception e) {
+	    ExceptionDialog d = new ExceptionDialog();	
+	    d.setException(e);
+	    d.showAndWait();	
+	}	
     }
 }
