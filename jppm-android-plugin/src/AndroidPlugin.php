28a2bba7edddad9a698ec3877ccc4958677d057d
Максим Тарасов <yousan4ik@gmail.com>
2018-09-11 02:13:43 +0300

    upd

--- jppm-android-plugin/src/AndroidPlugin.php
+++ jppm-android-plugin/src/AndroidPlugin.php
@@ -3,6 +3,9 @@ use packager\{
     cli\Console, Event, JavaExec, Packager, Vendor
 };
 use php\io\Stream;
+use php\lib\fs;
+use compress\ZipArchive;
+use compress\ZipArchiveEntry;
 
 /**
  * Class AndroidPlugin
@@ -11,7 +14,6 @@ use php\io\Stream;
  * @jppm-task build_apk as apk
  * @jppm-task build_adb as apk-adb
  * @jppm-task build_release as apk-release
- * @jppm-task test_desktop as desktop-run
  * @jppm-task init as init
  */
 class AndroidPlugin
@@ -19,10 +21,8 @@ class AndroidPlugin
     public function init(Event $e)
     {
         $packageData = $e->package()->toArray();
-        $androidData = $packageData['android'];
 
-        if (!isset($packageData['deps']['jphp-android-ext']))
-        {
+        if (!isset($packageData['deps']['jphp-android-ext'])) {
             Console::log('-> installing android extension ...');
 
             Tasks::run('add', [
@@ -36,62 +36,35 @@ class AndroidPlugin
 
         Console::log("-> prepare jphp compiler ...");
 
-        \php\lib\fs::makeDir('./.venity/');
-        \php\lib\fs::makeFile('./.venity/compiler.jar');
+        fs::makeDir('./.venity/');
+        fs::makeFile('./.venity/compiler.jar');
 
         Stream::putContents('./.venity/compiler.jar', Stream::getContents("res://compiler.jar"));
     }
 
     public function generateBuild(string $file, array $androidData)
     {
+        Console::log('-> generate build.gradle file ...');
 
-	    Console::log('-> generate build.gradle file ...');
+	    $buildScript  = "buildscript {\n";
+	    $buildScript .= "repositories {\n\r\rjcenter()\n}\n";
+	    $buildScript .= "dependencies {\n\rclasspath 'org.javafxports:jfxmobile-plugin:1.3.10'\n}\n";
+	    $buildScript .= "}\n\n";
+	    $buildScript .= "apply plugin: 'org.javafxports.jfxmobile'";
+	    $buildScript .= "repositories {\n\rmaven {\n";
+	    $buildScript .= "\rname 'gulon'\n\r\rurl \"http://nexus.gluonhq.com/nexus/content/repositories/releases/\"\n}";
+	    $buildScript .= "\rmavenLocal()\n\rjcenter()\n}\n";
+	    $buildScript .= "dependencies {\n   androidRuntime 'com.gluonhq:charm-down-core-android:3.5.0'\n";
+	    $buildScript .= "\rcompile files('build/{$file}')\n}\n";
+	    $buildScript .= "jfxmobile {\n  javafxportsVersion = '8.60.9'\n";
+	    $buildScript .= "\rdownConfig {\n\r\rversion = '3.8.0'\n\r\rplugins 'display', 'lifecycle', 'statusbar', 'storage'\n}\n";
+	    $buildScript .= "\randroid {\n\r\rcompileSdkVersion  = {$androidData['sdk']}\n\r\rbuildToolsVersion  = '{$androidData['sdk-tools']}'\n\r\rapplicationPackage = '{$androidData['package-name']}'\n }\n\n";
+	    $buildScript .= "mainClassName = \"org.venity.jphp.ext.android.UXAndroidApplication\"\n";
 
-	$buildScript = "buildscript {
-    repositories {
-        jcenter()
-    }
-    dependencies {
-        classpath 'org.javafxports:jfxmobile-plugin:1.3.10'
-    }
-}
+	    if (fs::isFile("./build.coustom.gradle"))
+	        $buildScript .= Stream::getContents("./build.coustom.gradle");
 
-apply plugin: 'org.javafxports.jfxmobile'
-
-repositories {
-    maven {
-        name 'gulon'
-        url \"http://nexus.gluonhq.com/nexus/content/repositories/releases/\" // gluon repository
-    }
-
-    mavenLocal()
-    jcenter()
-  
-}
-
-dependencies {
-    androidRuntime 'com.gluonhq:charm-down-core-android:3.5.0'
-    compile files('build/{$file}')
-}
-
-jfxmobile {
-    javafxportsVersion = '8.60.9'
-
-    downConfig {
-        version = '3.8.0'
-        plugins 'display', 'lifecycle', 'statusbar', 'storage'
-    }
-
-    android {
-        compileSdkVersion  = {$androidData['sdk']}
-        buildToolsVersion  = '{$androidData['sdk-tools']}'
-        applicationPackage = '{$androidData['package-name']}'
-    }
-}
-
-mainClassName = \"org.venity.jphp.ext.android.UXAndroidApplication\"";
-
-        \php\io\Stream::putContents('./build.gradle', $buildScript);
+        Stream::putContents('./build.gradle', $buildScript);
     }
 
     public function gradle_build(string $task, Event $event)
@@ -107,19 +80,17 @@ mainClassName = \"org.venity.jphp.ext.android.UXAndroidApplication\"";
 
         Console::log('-> unpack jar');
 
-        \php\lib\fs::makeDir('./build/out');
+        fs::makeDir('./build/out');
 
-        $zip = new \compress\ZipArchive(\php\lib\fs::abs('./build/' . $buildFileName . '.jar'));
-        $zip->readAll(function (\compress\ZipArchiveEntry $entry, ?Stream $stream) {
-            echo '.';
-            if (!$entry->isDirectory())
-            {
-                \php\lib\fs::makeFile(\php\lib\fs::abs('./build/out/' . $entry->name));
-                \php\lib\fs::copy($stream, \php\lib\fs::abs('./build/out/' . $entry->name));
-            }
-            else \php\lib\fs::makeDir(\php\lib\fs::abs('./build/out/' . $entry->name));
+        $zip = new ZipArchive(fs::abs('./build/' . $buildFileName . '.jar'));
+        $zip->readAll(function (ZipArchiveEntry $entry, ?Stream $stream) {
+            if (!$entry->isDirectory()) {
+                fs::makeFile(fs::abs('./build/out/' . $entry->name));
+                fs::copy($stream, fs::abs('./build/out/' . $entry->name));
+                echo '.';
+            } else fs::makeDir(fs::abs('./build/out/' . $entry->name));
         });
-        echo " done\n";
+        echo ". done\n";
 	    
         Console::log('-> starting compiler ...');
 
@@ -132,10 +103,9 @@ mainClassName = \"org.venity.jphp.ext.android.UXAndroidApplication\"";
         $exit = $process->inheritIO()->startAndWait()->getExitValue();
 
         if ($exit != 0) {
-            Console::log("[ERROR] Error compiling your хомно php code");
+            Console::log("[ERROR] Error compiling jphp");
             exit($exit);
-        } else
-            Console::log(" -> done");
+        } else Console::log(" -> done");
 
         $this->generateBuild('compile.jar', $androidData);
         
@@ -162,10 +132,5 @@ mainClassName = \"org.venity.jphp.ext.android.UXAndroidApplication\"";
     public function build_release(Event $event)
     {
         $this->gradle_build('androidRelease', $event);
-    }
-
-    public function test_desktop(Event $event)
-    {
-        $this->gradle_build('run', $event);
     }
 }
